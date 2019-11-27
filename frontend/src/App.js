4948847e5ec8c2c3584bcacb44898a021f4dae54
Victor <victor.aymard@edu.ece.fr>
2019-11-27 09:16:57 +0100

    gestion merge ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,27 +20,33 @@ class App extends React.Component {
                 </aside>
             </div>
             </body>
+
+            /*
+            * // form page
+            *
+            * <div className="App">
+                  <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                      <Form/>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn React
+                    </a>
+                  </header>
+               </div>
+    * */
         );
     }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Form/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
 }
 
 export default App;
