ca0278a6bc793cb328c8b938a5502c5572809e8d
Elias-nri <78907231+Elias-nri@users.noreply.github.com>
2021-12-06 00:11:35 +0100

    up

--- backend/CallAPI.js
+++ backend/CallAPI.js
@@ -1,19 +1,19 @@
 
 
-const fetchSearch = async() => {
-    temperatures = await fetch(`https://api.smartthings.com/v1/devices/f95f3581-f986-4670-ac63-94c6c0656d56/components/main/capabilities/temperatureMeasurement/status`)
-    .then(res => res.json())
-    .then(res => res.temperatures)
-    console.log(temperatures);
+const fetchSearch = async () => {
+    values = await fetch(`http://localhost:3001/posts/`)
+        .then(res => res.json())
+        .then(res => res.values)
+    console.log(values);
 }
 
-const affichage = async() => {
-    await fetchSearch(`https://api.smartthings.com/v1/devices/f95f3581-f986-4670-ac63-94c6c0656d56/components/main/capabilities/temperatureMeasurement/status`);
-    
+const affichage = async () => {
+    await fetchSearch()
+
     results.innerHTML = (
-        temperatures.map(temperature => (
-            console.log(temperature.value)        
-        ))           
+        values.map(value => (
+            console.log(value)
+        ))
     );
 };
 
