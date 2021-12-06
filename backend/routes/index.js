6b6c1a63ac3353b1343fd7cfb881905b75045147
Elias-nri <78907231+Elias-nri@users.noreply.github.com>
2021-12-06 20:38:47 +0100

    graph et bdd

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('../models/dbConfig');
 const postsRoutes = require('../routes/postsController');
+const postsRoutes1 = require('../routes/postsController1');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,7 +10,8 @@ const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/posts', postsRoutes);
+app.use('/enedis', postsRoutes);
+app.use('/temperature', postsRoutes1);
 
 app.listen(3001, () => console.log('Server started: 3001'));
 
